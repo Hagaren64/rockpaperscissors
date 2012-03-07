c17923e554f4bde7a8ebe69c082574ab564053b1
Julian Togashi <juliantogashi@gmail.com>
2012-03-07 01:07:26 -0800

    think I got RSPEC....

--- spec/controllers/throw_controller_spec.rb
+++ spec/controllers/throw_controller_spec.rb
@@ -29,27 +29,34 @@ describe ThrowController do
   	it "should display a win, lose, or tie" do
   		@defeat = {rock: :scissors, paper: :rock, scissors: :paper}
   		@throws = @defeat.keys
-  		@userthrow = @throws.sample
-  		get :userthrow, :type => @userthrow
+  		#@userthrow = :rock
+  		#get :userthrow, :type => @userthrow
 
       # The following grabs the 'actual' move made by the computer
       # comp_throw is not being controlled here, but the actual
       # controller
-  		@comp_throw = assigns(:computer_throw)
-   #   a=@throws
+  		#@comp_throw = assigns(:computer_throw)
+      a=@throws
+      i=0
       
-  #a.each {|x|
-    #  @userthrow=x
-     # print @userthrow, " -- ", @comp_throw
-      #get :userthrow, :type => @userthrow
+  a.each {|x|
+      @userthrow=x
+      get :userthrow, :type => @userthrow
+      @comp_throw = assigns(:computer_throw)
+      print @userthrow, " ", i, " -- ", @comp_throw, " "
+      i+=1
+
       if @userthrow==@comp_throw
   			response.should have_selector("h2", :content => "You tied.")
+  			print "T // "
       elsif @comp_throw == @defeat[@userthrow] 
   			response.should have_selector("h2", :content => "You Won!")
+  			print "W // "
       else
   			response.should have_selector("h2", :content => "You Lost!")
+  			print "L // "
     end
-  #}
+  }
 
   	end
     end
